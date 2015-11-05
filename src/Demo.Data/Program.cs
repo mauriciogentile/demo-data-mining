0ac491930e2a0c34dd732fc84fe83f9200170f08
mauriciog <mauriciog@digiterre.com>
2015-11-04 22:53:15 -0300

    adding desktop App

--- src/Demo.Data/Program.cs
+++ src/Demo.Data/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using FileHelpers;
 
 namespace Demo.Data
 {
@@ -8,13 +9,12 @@ namespace Demo.Data
     {
         static void Main(string[] args)
         {
+            var properties = new List<Property>();
             using (var context = new DemoDbContext(ConfigurationManager.ConnectionStrings["Demo"].ConnectionString))
             {
-                context.Database.ExecuteSqlCommand("DELETE FROM [Properties]");
+                //context.Database.ExecuteSqlCommand("DELETE FROM [Properties]");
 
                 int total = args.Length > 0 ? Convert.ToInt32(args[0]) : 100000;
-
-                var properties = new List<Property>();
 
                 for (int i = 0; i < total; i++)
                 {
@@ -36,9 +36,12 @@ namespace Demo.Data
                 context.Properties.AddRange(properties);
 
                 Console.WriteLine("Saving into database...");
-                context.SaveChanges();
+                //context.SaveChanges();
                 Console.WriteLine("Data saved!");
             }
+
+            var engine = new FileHelperEngine<Property>();
+            engine.WriteFile(string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}.csv", DateTime.Now), properties);
 
             Console.ResetColor();
             Console.WriteLine("Press any key to exit...");
---
+++ src/FileImporter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Demo.Data;
+using FileHelpers;
+using FileHelpers.Options;
+using Microsoft.Win32;
+
+namespace FileImporter
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void PickFile(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".csv",
+                Filter = "CSV Files (*.csv)|*.csv"
+            };
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                TextBox1.Text = filename;
+            }
+        }
+
+        void ProcessFile(object sender, RoutedEventArgs e)
+        {
+            var engine = new FileHelperEngine<Property>();
+            this.DataContext = engine.ReadFile(TextBox1.Text);
+        }
+    }
+}
